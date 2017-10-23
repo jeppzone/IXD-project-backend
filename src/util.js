d9e829e7b1214570e05e3d8f458c51524dbb3ede
Jesper Olsson <jespols@student.chalmers.se>
2017-10-23 23:52:22 +0200

    20 % of rainforest gone from beginning

--- src/util.js
+++ src/util.js
@@ -7,15 +7,17 @@ module.exports = {
 
 function createNewBoard(callback) {
   const array = []
-  for (var i = 0; i < 1000; i++){
-    array.push(0)
+  for (var i = 0; i < 800; i++){
+    array.push(0);
   }
+  for (var i = 0; i < 200; i++){
+    array.push(1);
+  }
+
+  shuffle(array);
 
   let temp = new Board({
     board: array,
-    nbrOfGamesPlayed: 0,
-    forestDestroyed: 0,
-    forestLeft: 10000
   })
 
   temp.save(() => {
@@ -27,4 +29,14 @@ function clearBoard(callback) {
   return Board.remove({}, ()=> {
     callback();
   });
+}
+
+function shuffle(a) {
+   var j, x, i;
+   for (i = a.length - 1; i > 0; i--) {
+       j = Math.floor(Math.random() * (i + 1));
+       x = a[i];
+       a[i] = a[j];
+       a[j] = x;
+   }
 }
